54b3a83a2cb51712a24a104f580e5617ea6791d2
tαngenx <tgxed@yandex.ru>
2022-10-16 22:12:47 +0300

    Update .gitignore

--- lib/globals.dart
+++
@@ -1,3 +0,0 @@
-import 'package:lfdi/logger.dart';
-
-final logger = Logger();
--- lib/utils/debounce.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-void Function() debounce(void Function() callback, Duration duration) {
-  Timer? timer;
-  return () {
-    timer?.cancel();
-    timer = Timer(duration, callback);
-  };
-}

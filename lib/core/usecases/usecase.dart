5e2f57c0d0229cd4c5b7b15cc35cfa770a8c873d
tαngenx <tgxed@yandex.ru>
2023-04-21 14:19:16 +0300

    chore(app): starting from scratch (again)

--- lib/core/usecases/usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:lfi/core/error/failure.dart';
-
-abstract class Usecase<Type, Params> {
-  Future<Either<Failure, Type>> call(Params params);
-}
--- lib/features/lastfm/presentation/bloc/lastfm_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'lastfm_bloc.dart';
-
-abstract class LastfmEvent extends Equatable {
-  const LastfmEvent();
-
-  @override
-  List<Object> get props => [];
-}

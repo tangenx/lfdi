21bbd5404049407581225805e3c55b50e9bfa5ae
tαngenx <me@tangenx.dev>
2025-10-14 06:39:41 +0300

    chore: some generated files

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=K:\fvm\versions\3.22.0"
-export "FLUTTER_APPLICATION_PATH=E:\CodeProjects\lfdi"
+export "FLUTTER_ROOT=E:\fvm\versions\3.22.0"
+export "FLUTTER_APPLICATION_PATH=G:\CodeProjects\lfdi"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0.9"
-export "FLUTTER_BUILD_NUMBER=1.0.0.9"
+export "FLUTTER_BUILD_NAME=1.0.0.10"
+export "FLUTTER_BUILD_NUMBER=1.0.0.10"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
